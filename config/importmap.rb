706382905e976e2d350e995d82a2bf67d85161bd
Muhammad Abdullah <mabdullah5514@yahoo.com>
2023-07-27 19:30:26 +0500

    resolve some turbolink issues

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,6 +5,8 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "review", to: 'review.js'
+pin "youtube", to: 'youtube.js'
 
 pin "popper", to: 'popper.js', preload: true
 
